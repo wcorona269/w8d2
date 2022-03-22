9295f593344c7b7a643bd6b554a1bc6ba528cd0b
App Academy Student <email@email.com>
2022-03-22 17:04:07 -0400

    finish phase 3

--- skeleton 2/phase_3_iteration.js
+++ skeleton 2/phase_3_iteration.js
@@ -1,19 +1,33 @@
 
-Array.prototype.myBsort = function () {
-  let sorted = false;
-  while (sorted === false) {
-    sorted = true;
+// Array.prototype.myBsort = function () {
+//   let sorted = false;
+//   while (sorted === false) {
+//     sorted = true;
 
-    for (let i = 0; i < this.length - 1; i++) {
-      let j = (i + 1);
-      if (this[i] > this[j]) {
-        [this[i], this[j]] = [this[j], this[i]];
-        sorted = false;
-      }
-    }
-  } return this;
+//     for (let i = 0; i < this.length - 1; i++) {
+//       let j = (i + 1);
+//       if (this[i] > this[j]) {
+//         [this[i], this[j]] = [this[j], this[i]];
+//         sorted = false;
+//       }
+//     }
+//   } return this;
+// }
+
+
+
+Array.prototype.substrings = function () {
+  let subs = []
+
+    for (let i = 0; i < this.length; i++) {
+      for (let j = (i + 1); j < this.length; j++) {
+        subs.push(this.slice(i, j));
+       }
+    } return subs;
 }
 
 let arr = [6, 5, 4, 57, 3, 85, 366, 23];
+let arr1 = ['helloworld'];
 
-console.log(arr.myBsort());
+console.log(arr.substrings());
+// console.log(arr1.substrings());
